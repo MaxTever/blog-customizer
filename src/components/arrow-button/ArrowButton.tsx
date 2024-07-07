47c5e236eb4543e142d297a879010e10b2725e3b
MaxTever <m2308902@edu.misis.ru>
2024-07-07 18:42:33 +0300

    Added base functionality

--- src/components/arrow-button/ArrowButton.tsx
+++ src/components/arrow-button/ArrowButton.tsx
@@ -1,19 +1,80 @@
 import arrow from 'src/images/arrow.svg';
-
+import React from 'react';
 import styles from './ArrowButton.module.scss';
+import clsx from 'clsx';
 
 /** Функция для обработки открытия/закрытия формы */
-export type OnClick = () => void;
 
-export const ArrowButton = () => {
+// export interface ArrowButtonProps {
+// 	onClick: (target: EventTarget) => void;
+// }
+
+// export const ArrowButton: React.FunctionComponent<ArrowButtonProps> = ({
+// 	onClick,
+// }) => {
+// 	const [click, setClick] = useState(false);
+
+// 	const handleClick = (event: React.MouseEvent) => {
+// 		onClick(event.target);
+// 		setClick(prevState => !prevState);
+// 	};
+
+// 	// const isClicked = click;
+// 	// const ArrowButtonClasses = {
+// 	// 	[styles.container_open]: isClicked === 'true',
+// 	// 	[styles.arrow_open]: isClicked === 'true'
+// 	// }
+
+// 	const isClicked = click;
+// 	const ArrowButtonClasses = clsx(styles.container, {
+// 		[styles.container_open]: isClicked,
+// 	});
+// 	const ArrowClasses = clsx(styles.arrow, { [styles.arrow_open]: isClicked });
+
+// 	return (
+// 		/* Не забываем указаывать role и aria-label атрибуты для интерактивных элементов */
+// 		<div
+// 			role='button'
+// 			aria-label='Открыть/Закрыть форму параметров статьи'
+// 			tabIndex={0}
+// 			className={ArrowButtonClasses}
+// 			onClick={handleClick}>
+// 			<img src={arrow} alt='иконка стрелочки' className={ArrowClasses} />
+// 		</div>
+// 	);
+// };
+
+export type ArrowButtonProps = {
+	onClick: (target: EventTarget) => void;
+	openState: boolean;
+	setOpen: (value: boolean) => void;
+};
+
+export const ArrowButton: React.FunctionComponent<ArrowButtonProps> = ({
+	openState,
+	setOpen,
+	onClick,
+}) => {
+	const handleClick = (event: React.MouseEvent) => {
+		onClick(event.target);
+		setOpen(!openState);
+	};
+
+	const isClicked = openState;
+	const ArrowButtonClasses = clsx(styles.container, {
+		[styles.container_open]: isClicked,
+	});
+	const ArrowClasses = clsx(styles.arrow, { [styles.arrow_open]: isClicked });
+
 	return (
 		/* Не забываем указаывать role и aria-label атрибуты для интерактивных элементов */
 		<div
 			role='button'
 			aria-label='Открыть/Закрыть форму параметров статьи'
 			tabIndex={0}
-			className={styles.container}>
-			<img src={arrow} alt='иконка стрелочки' className={styles.arrow} />
+			className={ArrowButtonClasses}
+			onClick={handleClick}>
+			<img src={arrow} alt='иконка стрелочки' className={ArrowClasses} />
 		</div>
 	);
 };
--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,20 +1,130 @@
 import { ArrowButton } from 'components/arrow-button';
 import { Button } from 'components/button';
+import { Select } from 'components/select';
+import {
+	fontFamilyOptions,
+	fontColors,
+	backgroundColors,
+	fontSizeOptions,
+	defaultArticleState,
+	contentWidthArr,
+	ArticleStateType,
+	OptionType,
+} from 'src/constants/articleProps';
+import { RadioGroup } from '../radio-group';
+import { useEffect, useState, useRef } from 'react';
+import { Separator } from '../separator';
+import { Text } from '../text';
+import { useOutsideClickClose } from '../select/hooks/useOutsideClickClose';
 
 import styles from './ArticleParamsForm.module.scss';
 
-export const ArticleParamsForm = () => {
+type ArticleParamsFormProps = {
+	appState: ArticleStateType;
+	setAppState: (param: ArticleStateType) => void;
+};
+
+export const ArticleParamsForm: React.FunctionComponent<
+	ArticleParamsFormProps
+> = ({ appState, setAppState }) => {
+	const [openState, setOpen] = useState(false);
+	const formRef = useRef<HTMLElement | null>(null);
+	const [fieldformState, setFormState] = useState<ArticleStateType>(appState);
+	const closeRef = useRef<HTMLDivElement | null>(null);
+
+	const handleClick = () => {
+		setOpen(!openState);
+	};
+
+	useOutsideClickClose({
+		isOpen: openState,
+		rootRef: closeRef,
+		onClose: () => {
+			setOpen(openState);
+		},
+		onChange: (newValue) => {
+			setOpen(newValue);
+		},
+	});
+
+	useEffect(() => {
+		if (openState) {
+			formRef.current?.classList.add(styles.container_open);
+		} else {
+			formRef.current?.classList.remove(styles.container_open);
+		}
+	}, [openState]);
+
+	const handleChangeSelect = (
+		option: OptionType,
+		optionKey: keyof ArticleStateType
+	) => setFormState({ ...fieldformState, [optionKey]: option });
+
+	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		setAppState(fieldformState);
+	};
+
+	const clearFormState = () => {
+		setFormState(defaultArticleState);
+		setAppState(defaultArticleState);
+	};
+
 	return (
-		<>
-			<ArrowButton />
-			<aside className={styles.container}>
-				<form className={styles.form}>
+		<div ref={closeRef}>
+			<ArrowButton
+				onClick={handleClick}
+				openState={openState}
+				setOpen={setOpen}
+			/>
+			<aside ref={formRef} className={styles.container}>
+				<form className={styles.form} onSubmit={handleSubmit}>
+					<Text as='h1' size={31} weight={800} uppercase dynamicLite={false}>
+						Задайте параметры
+					</Text>
+					<Select
+						selected={fieldformState.fontFamilyOption}
+						options={fontFamilyOptions}
+						title={'Шрифт'}
+						onChange={(option) =>
+							handleChangeSelect(option, 'fontFamilyOption')
+						}></Select>
+					<RadioGroup
+						name={'Шрифты'}
+						options={fontSizeOptions}
+						selected={fieldformState.fontSizeOption}
+						title={'Размер шрифта'}
+						onChange={(option) =>
+							handleChangeSelect(option, 'fontSizeOption')
+						}></RadioGroup>
+					<Select
+						selected={fieldformState.fontColor}
+						options={fontColors}
+						title={'Цвет шрифта'}
+						onChange={(option) =>
+							handleChangeSelect(option, 'fontColor')
+						}></Select>
+					<Separator />
+					<Select
+						selected={fieldformState.backgroundColor}
+						options={backgroundColors}
+						title={'Цвет фона'}
+						onChange={(option) =>
+							handleChangeSelect(option, 'backgroundColor')
+						}></Select>
+					<Select
+						selected={fieldformState.contentWidth}
+						options={contentWidthArr}
+						title={'Ширина контента'}
+						onChange={(option) =>
+							handleChangeSelect(option, 'contentWidth')
+						}></Select>
 					<div className={styles.bottomContainer}>
-						<Button title='Сбросить' type='reset' />
+						<Button title='Сбросить' type='reset' onClick={clearFormState} />
 						<Button title='Применить' type='submit' />
 					</div>
 				</form>
 			</aside>
-		</>
+		</div>
 	);
 };
